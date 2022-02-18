aa5e1cac1caca5512dc1f6f1ddc93aaeae46177e
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-02-17 22:42:26 -0300

    finaliza programa Lista de tarefa

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿bool sair = false;
+﻿using Tarefas.db;
+
+bool sair = false;
 while (!sair)
 {
     string opcao = UI.SelecionaOpcaoEmMenu();
@@ -30,37 +32,111 @@ while (!sair)
 void ListarTodasAsTarefas()
 {
     UI.ExibeDestaque("\n-- Listar todas as Tarefas ---");
-    // Continue daqui
+    
+    using (var _db = new tarefasContext())
+    {
+        var TodasTarefas = _db.Tarefa.ToList<Tarefa>();
+
+        int quantidadeTarefas = TodasTarefas.Count();
+        Console.WriteLine($"Temos: {quantidadeTarefas} tarefa(s)");
+
+        foreach (var tarefa in TodasTarefas)
+
+        {
+            string feito = tarefa.Concluida ? "x"  : " ";
+            Console.WriteLine($"\n[{feito}] {tarefa.Id} => {tarefa.Descricao}");
+        }
+        
+    }
 }
 
 void ListarTarefasPendentes()
 {
     UI.ExibeDestaque("\n-- Listar Tarefas Pendentes ---");
-    // Continue daqui
+
+    using (var _db = new tarefasContext())
+    {
+        var TodasTarefas = _db.Tarefa.Where(t => !t.Concluida).ToList<Tarefa>();
+
+        int quantidadeTarefas = TodasTarefas.Count();
+        Console.WriteLine($"Temos: {quantidadeTarefas} tarefa(s)");
+
+        foreach (var tarefa in TodasTarefas)
+
+        {
+            string feito = tarefa.Concluida ? "x"  : " ";
+            Console.WriteLine($"\n[{feito}] {tarefa.Id} => {tarefa.Descricao}");
+        }
+    }
 }
 
 void ListarTarefasPorDescricao()
 {
     UI.ExibeDestaque("\n-- Listar Tarefas por Descrição ---");
     string descricao = UI.SelecionaDescricao();
-    // Continue daqui
-    Console.WriteLine(descricao);
+
+    using (var _db = new tarefasContext())
+    {
+        var TodasTarefas = _db.Tarefa.Where(t => t.Descricao.Contains(descricao))
+        .OrderBy(t => t.Descricao)
+        .ToList<Tarefa>();
+
+        int quantidadeTarefas = TodasTarefas.Count();
+        Console.WriteLine($"Temos: {quantidadeTarefas} tarefa(s)");
+
+        foreach (var tarefa in TodasTarefas)
+
+        {
+            string feito = tarefa.Concluida ? "x"  : " ";
+            Console.WriteLine($"\n[{feito}] {tarefa.Id} => {tarefa.Descricao}");
+        }
+    }
 }
 
 void ListarTarefasPorId()
 {
     UI.ExibeDestaque("\n-- Listar Tarefas por Id ---");
     int id = UI.SelecionaId();
-    // Continue daqui
-    Console.WriteLine(id);
+    
+    using (var _db = new tarefasContext())
+    {
+        var tarefa = _db.Tarefa.Find(id);
+
+        if (tarefa == null)
+        {
+            Console.WriteLine("Esse Id não diz respeito a nenhuma tarefa. ");
+            return;
+        }
+
+        string feito = tarefa.Concluida ? "x"  : " ";
+        Console.WriteLine($"\n[{feito}] {tarefa.Id} => {tarefa.Descricao}");
+    }
 }
 
 void IncluirNovaTarefa()
 {
     UI.ExibeDestaque("\n-- Incluir Nova Tarefa ---");
     string descricao = UI.SelecionaDescricao();
-    // Continue daqui
-    Console.WriteLine(descricao);
+    
+    if (String.IsNullOrEmpty(descricao))
+    {
+        Console.WriteLine("Preciso de uma descrição para cadrastar a tarefa");
+        return;
+    }
+
+    using (var _db = new tarefasContext())
+    {
+        var tarefa = new Tarefa {
+            Descricao = descricao
+        };
+
+        _db.Tarefa.Add(tarefa);
+        _db.SaveChanges();
+
+        string feito = tarefa.Concluida ? "x"  : " ";
+        Console.WriteLine($"\n[{feito}] {tarefa.Id} => {tarefa.Descricao}");
+    }
+
 }
 
 void AlterarDescricaoDaTarefa()
